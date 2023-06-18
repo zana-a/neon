7d1667805c188769483a76ce8963855bfd9aad27
zana-a <zana200.a@gmail.com>
2023-06-18 13:55:56 +0100

    Use Arg constructor instead of macro

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -33,24 +33,27 @@ impl Cli {
     fn create_new_command() -> Command {
         Command::new(Self::COMMAND_NEW)
             .about("Create a new neon package")
-            .arg(arg!(<name>))
+            .arg(
+                Arg::new("path")
+                    .required(true)
+                    .action(ArgAction::Set)
+                    .help("The path location of the new project"),
+            )
     }
 
     fn create_build_command() -> Command {
-        Command::new(Self::COMMAND_BUILD)
-            .about("Build a local neon package")
-            .arg(arg!(<path>))
+        Command::new(Self::COMMAND_BUILD).about("Build a local neon package")
     }
 
     fn create_clean_command() -> Command {
-        Command::new(Self::COMMAND_CLEAN)
-            .about("Remove artifacts that neon has generated")
-            .arg(arg!(<path>))
+        Command::new(Self::COMMAND_CLEAN).about("Remove artifacts that neon has generated")
     }
 
     fn handle_new_command(root_command: &ArgMatches) {
-        if root_command.subcommand_matches(Self::COMMAND_NEW).is_some() {
-            println!("Creating new project");
+        if let Some(command) = root_command.subcommand_matches(Self::COMMAND_NEW) {
+            if let Some(path) = command.get_one::<String>("path") {
+                println!("creating project in: {}", path)
+            }
         }
     }
 
