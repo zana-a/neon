e654b4be21875a54a60404326df8776c01b8e89e
Zana <zana200@icloud.com>
2023-10-05 23:10:58 +0000

    chore: clean padded functions

--- src/core/parser/padded.rs
+++ src/core/parser/padded.rs
@@ -5,20 +5,18 @@ use nom::{
     IResult, Parser,
 };
 
-pub fn padded0<'a, F, O, E: ParseError<&'a str>>(
-    inner: F,
-) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+pub fn padded0<'a, F, O, E>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
 where
     F: Parser<&'a str, O, E>,
+    E: ParseError<&'a str>,
 {
     delimited(multispace0, inner, multispace0)
 }
 
-pub fn padded1<'a, F, O, E: ParseError<&'a str>>(
-    inner: F,
-) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+pub fn padded1<'a, F, O, E>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
 where
     F: Parser<&'a str, O, E>,
+    E: ParseError<&'a str>,
 {
     delimited(multispace1, inner, multispace1)
 }
